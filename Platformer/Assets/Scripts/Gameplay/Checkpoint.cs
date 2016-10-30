ea9a7f3ca906839a43f13e365cc6c5f778bde9aa
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-10-30 15:06:09 +0100

    Checkpoint spawning.

---
+++ Platformer/Assets/Scripts/Gameplay/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+    public void Awake()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<Collider2D>().enabled = false;
+            levelManager.SetCheckpoint(transform);
+        }
+    }
+}
--- Platformer/Assets/Scripts/Gameplay/LevelManager.cs
+++ Platformer/Assets/Scripts/Gameplay/LevelManager.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
+public class LevelManager : MonoBehaviour
+{
+    // spawning
+    public GameObject PlayerPrefab;
+    private GameObject player;
+    private Transform currentCheckpoint;
+    public Transform startPoint;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // lives & score
+    public int lives = 3;
+
+    public void Awake()
+    {
+        currentCheckpoint = startPoint;
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        player = Instantiate(PlayerPrefab, currentCheckpoint.position, currentCheckpoint.rotation) as GameObject;
+    }
+
+    public void SetCheckpoint(Transform t)
+    {
+        currentCheckpoint = t;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player)
+        {
+            SpawnPlayer();
+            --lives;
+        }
+    }
 }
