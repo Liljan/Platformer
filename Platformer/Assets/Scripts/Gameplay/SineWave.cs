ac9d5722c5210c10b57f82e0c1f72072f675e5e4
Liljan <marcus.t.lilja@gmail.com>
2016-11-01 23:14:57 +0100

    Trigonometry movement.

---
+++ Platformer/Assets/Scripts/Gameplay/SineWave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineWave : MonoBehaviour
+{
+    public Vector2 amp;
+    private float phase = Mathf.PI/2;
+    public float speed = 1.0f;
+
+    private Vector3 startPos;
+
+    public void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPos + new Vector3(amp.x * Mathf.Cos(phase), amp.y * Mathf.Sin(phase), 0.0f);
+        phase += speed * Time.deltaTime;
+    }
+}
